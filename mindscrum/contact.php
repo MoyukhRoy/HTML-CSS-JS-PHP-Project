feeddc2be8e380f1e416b61b5237ba8fe2ca1ad2
MoyukhRoy <moyukhroyniitsbz@gmail.com>
2023-09-16 09:56:37 +0530

    add

---
+++ mindscrum/contact.php
@@ -0,0 +1,126 @@
+<?php
+
+include 'components/connect.php';
+
+if(isset($_COOKIE['user_id'])){
+   $user_id = $_COOKIE['user_id'];
+}else{
+   $user_id = '';
+}
+
+if(isset($_POST['submit'])){
+
+   $name = $_POST['name']; 
+   $name = filter_var($name, FILTER_SANITIZE_STRING);
+   $email = $_POST['email']; 
+   $email = filter_var($email, FILTER_SANITIZE_STRING);
+   $number = $_POST['number']; 
+   $number = filter_var($number, FILTER_SANITIZE_STRING);
+   $msg = $_POST['msg']; 
+   $msg = filter_var($msg, FILTER_SANITIZE_STRING);
+
+   $select_contact = $conn->prepare("SELECT * FROM `contact` WHERE name = ? AND email = ? AND number = ? AND message = ?");
+   $select_contact->execute([$name, $email, $number, $msg]);
+
+   if($select_contact->rowCount() > 0){
+      $message[] = 'message sent already!';
+   }else{
+      $insert_message = $conn->prepare("INSERT INTO `contact`(name, email, number, message) VALUES(?,?,?,?)");
+      $insert_message->execute([$name, $email, $number, $msg]);
+      $message[] = 'message sent successfully!';
+   }
+
+}
+
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>contact us</title>
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+<?php include 'components/user_header.php'; ?>
+
+<section class="contact">
+
+   <div class="row">
+
+      <div class="image">
+         <img src="images/contact-img.svg" alt="">
+      </div>
+
+      <form action="" method="post">
+         <h3>get in touch</h3>
+         <input type="text" placeholder="enter your name" name="name" required maxlength="50" class="box">
+         <input type="email" placeholder="enter your email" name="email" required maxlength="50" class="box">
+         <input type="number" placeholder="enter your number" name="number" required maxlength="50" class="box">
+         <textarea name="msg" class="box" placeholder="enter your message" required maxlength="1000" cols="30" rows="10"></textarea>
+         <input type="submit" value="send message" class="inline-btn" name="submit">
+      </form>
+
+   </div>
+
+   <div class="box-container">
+
+      <div class="box">
+         <i class="fas fa-phone"></i>
+         <h3>phone number</h3>
+         <a href="tel:1234567890">123-456-7890</a>
+         <a href="tel:1112223333">111-222-3333</a>
+      </div>
+      
+      <div class="box">
+         <i class="fas fa-envelope"></i>
+         <h3>email address</h3>
+         <a href="mailto:mindscum@gmail.com">mindscum@gmail.come</a>
+         <a href="mailto:sandiproy@gmail.com">sandiproy@gmail.come</a>
+      </div>
+
+      <div class="box">
+         <i class="fas fa-map-marker-alt"></i>
+         <h3>office address</h3>
+         <a href="https://www.google.com/maps/dir/22.6420715,88.3756085/niit+shyabazer/@22.5946404,88.3334775,12.56z/data=!4m9!4m8!1m1!4e1!1m5!1m1!1s0x3a0276259a3455cd:0x1eaa236fca71f5ed!2m2!1d88.3732473!2d22.6032366">NIIT Kolkata Shyambazar Centre, 3rd Floor, NIIT Building, 123, Bidhan Sarani Rd, beside Bag Bazaar Bata, Shyam Bazar, Kolkata, West Bengal 700004</a>
+      </div>
+
+   </div>
+
+</section>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<footer class="footer">
+
+   &copy; copyright @ 2023 by <span>MINDSCRUM PVT. LTD.</span> | all rights reserved!
+
+</footer>
+
+<!-- custom js file link  -->
+<script src="js/script.js"></script>
+
+   
+</body>
+</html>
